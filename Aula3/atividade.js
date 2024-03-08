13d2cba82ff564d5413dc25ffbb756d36630d260
EduardoxxMenezes <162368836+EduardoxxMenezes@users.noreply.github.com>
2024-03-08 17:09:08 -0300

    Add files via upload

---
+++ Aula3/atividade.js
@@ -0,0 +1,33 @@
+var name = "Eduardo"
+let age = "15"
+const studing = true
+
+if(studing == true)
+{
+console.log("me chamo",name,"tenho", age,"anos", "e sou estudante!")
+}
+else
+{console.log("me chamo", name, "tenho",age, "anos e não estudo.")}
+
+const aham = "se mata"
+
+var treco = typeof aham
+console.log(treco)
+
+
+var serie = "How i met your mother"
+let temporadas = 9
+
+console.log("minha serie favorita é",serie,typeof serie,"ela tem",temporadas, "temporadas", typeof temporadas)
+//como usa prompt???
+
+var num = 4
+var pal = num.toString()
+console.log(typeof pal)
+var nao = Number(pal)
+console.log(typeof nao)
+
+const num1 = "186"
+const num2 = "769"
+
+console.log(Number(num1)+Number(num2))
---
+++ Aula3/auola3.js
@@ -0,0 +1,17 @@
+var sim = true
+let não = false
+var name = prompt("what is your name?");
+
+
+while(true)
+
+
+if (sla >= 5)
+{console.log("yea")}
+else 
+{}
+
+
+var cebolastristes =  "eduardo"
+let jeferson = 15
+console.log('HOLA, me llamo',cebolastristes,"tengo", jeferson, "años!")

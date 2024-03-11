faefc9b324cc346e9726b2830f7f518531f4fc84
EduardoxxMenezes <162368836+EduardoxxMenezes@users.noreply.github.com>
2024-03-11 16:26:05 -0300

    Add files via upload

---
+++ aula 4/aula4.js
@@ -0,0 +1,51 @@
+
+//a média dos alunos será considerada boa acima de 9.
+
+//TODOS os alunos devem ser listados neste software.
+
+//devem ser apresentados os horários de cada matéria.
+
+//os alunos deverão ser divididos por série e turma.
+
+//deverá haver uma aba que apresente os eventos da escola.
+
+//o software deverá apresentar as fichas dos alunos.
+
+//nas escolas humildes os alunos terem acesso a um auxilho do governo.
+
+//as escolas mais desafortunadas receberão bolsas 70% de 20% para faculdades.
+
+//as médias das escolas humildes deverão ser 6.
+
+//deverá ter uma interface para os pais cadastrarem os filhos online.
+
+//NF
+
+//deve haver uma aba para listar os alunos aprovados e reprovados em cada matéria.
+
+//no grêmio terá no máximo 6/7 pessoas.
+
+//caso um aluno não traga os materiais necessarios ele será punido com uma advertencia.
+
+//o software só poderá ser aberto após as 7:00.
+
+//só poderá entrar no máximo 30/35 pessoas no software,simultaneamente.
+
+//o software deve ser feito em JavaScript.
+
+//O software deve ter suporte para aparelhos móveis e computadores.
+
+//O sistema deve ser executado em sistemas operacionais windows 7 acima
+
+//REGRAS DE NEGOCIO
+
+//a média da escola santa katia do sul será 7.0 por motivos não declarados.
+
+//as advertencias maximas por aluno nas escolas é 2.
+
+//escolas particulares não poderão perdir o pagamento adiantado.
+
+//escolas particulares terão que fazer 2 parcelas de pelo menos R$ 500,00 nos primeiros 2 meses.
+
+
+

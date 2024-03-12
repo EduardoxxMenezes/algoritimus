dea523fae5024069958cd21f2f0d6d232dd9c1ad
EduardoxxMenezes <162368836+EduardoxxMenezes@users.noreply.github.com>
2024-03-12 16:51:32 -0300

    Add files via upload

---
+++ aula5/aula53.js
@@ -0,0 +1,12 @@
+var name = "marcio"
+var age = 19
+console.log(typeof name, typeof age)
+//nós não demos um valor as variaveis, assim, a maquina não sabe seu tipo, dando a ela o tipo "undefined"
+
+//the variables has no values cause we haven't place any information on it, this way, the computer places "undefined" as its type.
+
+//ao colocar o valor nas variaveis ele imprime "number" e "string" pois agora ele tem um número e um texto para indicar seu tipo.
+
+//when we place a value on the variables it prints "number" and "string" because the machine has a number and a text to define its kind
+
+console.log("Hello", name, "it is a pleasure to meet you, it has come to my ears that your ", age, "years old, am i right?")
---
+++ aula5/operadores/divisao.js
@@ -0,0 +1,5 @@
+let first = 87
+let second = 32
+
+console.log(first / second)
+console.log(first % second)

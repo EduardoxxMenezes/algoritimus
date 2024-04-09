d17da929bd96cc4e17d1217b84d9429b35ca235e
EduardoxxMenezes <162368836+EduardoxxMenezes@users.noreply.github.com>
2024-04-09 17:10:06 -0300

    Add files via upload

---
+++ aula12/FOR.js
@@ -0,0 +1,11 @@
+//FOR\\
+
+for(let i = 0; i < 10; i++){console.log(i)}
+//Eles simplificam a escrita de whiles.
+//é composto de começo, condição e final.
+
+const numeros = [1,2,3,4,5,6]
+for(let a = 0; a < numeros.length; a = a + 1){//não é 1 é "a"
+    const elemento = numeros[a]
+    console.log(elemento)
+}
---
+++ aula12/for2.js
@@ -0,0 +1,17 @@
+let maior1 = 0
+let numeros = [1,5,76,24,1]
+
+console.log("teste")
+
+  
+ 
+
+function teste(lista){
+    let grande = 0
+    for(let desisto; desisto < array.length; i ++)
+    {
+        if(lista[desisto] > grande){grande = lista[desisto]}
+    }
+    return grande
+}
+console.log(teste(numeros))
